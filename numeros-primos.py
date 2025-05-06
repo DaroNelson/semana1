69ec90648ae653745c22b2a70b7df307579ddb6d
DaroNelson <churydaro@gmail.com>
2025-05-06 17:26:07 -0300

    numero primos

---
+++ numeros-primos.py
@@ -0,0 +1,15 @@
+# funcion para verificar 
+def num_primos (n):
+    if n < 2:
+        return False
+# verifica la raiz cuadrada  
+    for i in range(2, int(n**0.5)+1): 
+        if n % i == 0:
+            return False      
+    return True
+# listra para obtener numeros primos 
+primos = [n for n in range(1, 1001) if num_primos(n)]
+#mostrar lista 
+print(primos)
+   
+
